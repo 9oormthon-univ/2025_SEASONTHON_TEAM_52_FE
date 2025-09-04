8c6f3e51863f99ec05182a24045af860d66faa79
stampHo <inho8024s@gmail.com>
2025-09-05 00:25:48 +0900

    [FEAT] add brand color palette (main, sub, black scale)

---
+++ app/styles/colors.ts
@@ -0,0 +1,16 @@
+// app/styles/colors.ts
+const colors = {
+  mainColor: "#FF6A00", // 주황색 (메인 브랜드 컬러)
+  mainSub1: "#FFDEC7",
+  mainSub2: "#FFEFE2",
+  subColor: "#3A87FF", // 보조 색
+  black: "#1F1F1F",
+  blackSub1: "#8C8C8C",
+  blackSub2: "#C5C5C5",
+  blackSub3: "#D8D8D8",
+  blackSub4: "#E6E6E6",
+  blackSub5: "#F9F9F9",
+  white: "#ffffff",
+};
+
+export default colors;
