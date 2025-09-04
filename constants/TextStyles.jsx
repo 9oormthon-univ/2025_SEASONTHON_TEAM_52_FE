35c60689b0abb8933a95b8c4e07ff1aafa6160e1
bhyerain <seunghye0307@naver.com>
2025-09-04 14:51:06 +0900

    Style: Common Text Style

--- constants/TextStyles.jsx
+++ constants/TextStyles.jsx
@@ -1,3 +1,62 @@
 import { StyleSheet } from "react-native";
 
-export const TEXT = StyleSheet.create({});
+export const TEXT = StyleSheet.create({
+  // 로고
+  logo: {
+    fontFamily: "Ownglyph",
+  },
+  // 헤드라인 1
+  head1: {
+    fontFamily: "Pretendard",
+    fontWeight: 700,
+    fontSize: 32,
+    lineHeight: 40,
+  },
+  // 헤드라인 2
+  head2: {
+    fontFamily: "Pretendard",
+    fontWeight: 700,
+    fontSize: 24,
+    lineHeight: 32,
+  },
+  // 타이틀
+  title1: {
+    fontFamily: "Pretendard",
+    fontWeight: 600,
+    fontSize: 20,
+    lineHeight: 28,
+    letterSpacing: -2,
+  },
+  // body
+  body1: {
+    fontFamily: "Pretendard",
+    fontWeight: 600,
+    fontSize: 18,
+    lineHeight: 24,
+  },
+  body2: {
+    fontFamily: "Pretendard",
+    fontWeight: 600,
+    fontSize: 16,
+    lineHeight: 24,
+  },
+  // body2.5
+  body22: {
+    fontFamily: "Pretendard",
+    fontWeight: 500,
+    fontSize: 16,
+    lineHeight: 24,
+  },
+  body3: {
+    fontFamily: "Pretendard",
+    fontWeight: 500,
+    fontSize: 14,
+    lineHeight: 24,
+  },
+  body4: {
+    fontFamily: "Pretendard",
+    fontWeight: 500,
+    fontSize: 12,
+    lineHeight: "150%",
+  },
+});
